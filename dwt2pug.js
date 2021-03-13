c42a8bdda53e442e44ccfe183516d4579124fa2e
Albir Tarsha <a.tarsha@yahoo.com>
2021-03-13 15:25:30 -0600

    created html2pugWorkaround()
    Moved the html2pug workaround code into its own function.

--- dwt2pug.js
+++ dwt2pug.js
@@ -5,6 +5,22 @@ const pug = html2pug(html, { tabs: true })
 
 const fs = require('fs')
 
+function html2pugWorkaround(pug) {
+  // The following code works around a bug in html2pug
+  const pugOut = pug.split('\n');
+  const regexA = /^\s+\|\s+$/;
+  const regexB = /^\S/;
+  let indxB = pugOut.length - 1;
+  for (let indxA = indxB - 1; indxA >= 0; indxA--, indxB--) {
+    if (regexA.test(pugOut[indxA]) &&
+        regexB.test(pugOut[indxB])) {
+      pugOut[indxA] += pugOut[indxB];
+      pugOut.splice(indxB, 1);
+    }
+  }
+  return pugOut.join('\n');
+}
+
 module.exports = function (dwtFilePath, pugFilePath) {
   try {
     if (!fs.existsSync(dwtFilePath)) {
@@ -23,21 +39,7 @@ module.exports = function (dwtFilePath, pugFilePath) {
       if (err) throw err;
       let pug = html2pug(data, { tabs: true });
       console.log('html2pug executed');
-      // Start Workaround
-      // The following code works around a bug in html2pug
-      const pugOut = pug.split('\n');
-      const regexA = /^\s+\|\s+$/;
-      const regexB = /^\S/;
-      let indxB = pugOut.length - 1;
-      for (let indxA = indxB - 1; indxA >= 0; indxA--, indxB--) {
-        if (regexA.test(pugOut[indxA]) &&
-            regexB.test(pugOut[indxB])) {
-          pugOut[indxA] += pugOut[indxB];
-          pugOut.splice(indxB, 1);
-        }
-      }
-      pug = pugOut.join('\n');
-      // End Workaround
+      pug = html2pugWorkaround(pug);
       try {
         fs.writeFile(pugFilePath, pug, 'utf8', (err) => {
           if (err) throw err;

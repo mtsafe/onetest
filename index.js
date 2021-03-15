41a9a7c166fc6ed4aaedddaa479b408784a04d6e
Albir Tarsha <a.tarsha@yahoo.com>
2021-03-14 23:57:12 -0500

    added method-override
    allows for PUT and DELETE

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const db = require('./config/db');
 const dotenv = require('dotenv');
 const express = require('express');
+const methodOverride = require('method-override');
 const path = require('path');
 // const bodyParser = require('body-parser')
 
@@ -26,6 +27,16 @@ app.use(express.urlencoded({ extended: true }));
 // Parse JSON bodies for this app. Make sure you put
 // `app.use(express.json())` **before** your route handlers!
 app.use(express.json());
+
+// Method Override allows PUT and DELETE
+app.use(methodOverride(function (req, res) {
+  if (req.body && typeof req.body === 'object' && '_method' in req.body) {
+    // look in urlencoded POST bodies and delete it
+    let method = req.body._method
+    delete req.body._method
+    return method
+  }
+}))
 
 // Routes
 app.use('/blogs', require('./routes'));

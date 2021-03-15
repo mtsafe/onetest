da4d7c863f384259fed0f4728b7220f95c78f849
Albir Tarsha <a.tarsha@yahoo.com>
2021-03-14 20:02:12 -0500

    organizing pug files
    su_* files are for authorized users only

--- routes.js
+++ routes.js
@@ -1,18 +1,19 @@
 const express = require('express');
 const router = express.Router();
+const path = require('path');
 const dwt2pug = require('./dwt2pug');
 
 // Landing page
 router.get('/1taat_login', (req,res) => {
   console.log('GET to /blogs/1taat_login');
-  res.render('login');
+  res.render('su_login');
 });
 
-// Dashboard
-router.get('/dashboard', (req,res) => {
-  console.log('GET to /blogs/dashboard');
-  res.render('dashboard');
-});
+// // Dashboard
+// router.get('/dashboard', (req,res) => {
+//   console.log('GET to /blogs/dashboard');
+//   res.render('dashboard');
+// });
 
 // Bring in DB Models
 let Blog = require('./models/blog');
@@ -27,7 +28,7 @@ router.get('/list', (req, res) => {
       return;
     }
     res.render('list_blog', {
-      title: 'Blogs',
+      page_title: 'Blogs',
       blogs: blogs
     });
   });
@@ -35,7 +36,39 @@ router.get('/list', (req, res) => {
 
 // View a single Blog post
 router.get('/:id', (req, res) => {
-  console.log('GET to /blogs/:id');
+  let ID = req.params.id;
+  console.log('GET to /blogs/:id _id='+ID);
+  if (!ID.match(/^[0-9a-fA-F]{24}$/)) {
+    console.log(`"${ID}" invalid ObjectId, cannot findById`);
+    console.log(`Sending 404: ${path.join(__dirname+"/public/error/404page.html")}`);
+    res.status(404).sendFile(path.join(__dirname+'/public/error/404page.html'));
+    return;
+  }
+  Blog.findById(ID, (err, blog) => {
+    if (err) {
+      console.log(err);
+      return;
+    }
+    // console.log(blog);
+    res.render('view_blog', {
+      page_title: 'Blog',
+      blog: blog
+    });
+  });
+});
+
+// Add a Blog Article
+router.get('/add', (req, res) => {
+  console.log('GET to /blogs/add');
+  dwt2pug('./Templates/main.dwt', './views/main.pug');
+  res.render('su_add_blog', {
+    page_title: 'Adding A Blog Article'
+  });
+});
+
+// Edit a single Blog post
+router.get('/edit/:id', (req, res) => {
+  console.log('GET to /blogs/edit/:id');
   let ID = req.params.id;
   Blog.findById(ID, (err, blog) => {
     if (!ID.match(/^[0-9a-fA-F]{24}$/)) {
@@ -46,21 +79,10 @@ router.get('/:id', (req, res) => {
       return;
     }
     // console.log(blog);
-    res.render('view_blog', {
-      title: 'Blog',
-      blog_title: blog.title,
-      blog_author: blog.author,
-      blog_body: blog.body
+    res.render('su_edit_blog', {
+      page_title: 'Edit Blog',
+      blog: blog
     });
-  });
-});
-
-// Add a Blog Article
-router.get('/add', (req, res) => {
-  console.log('GET to /blogs/add');
-  dwt2pug('./Templates/main.dwt', './views/main.pug');
-  res.render('add_blog', {
-    title: 'Adding A Blog Article'
   });
 });
 

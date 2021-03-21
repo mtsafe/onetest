8d6a2cd4a7ff73bf517d9a4c44620f7a758b6e32
Albir Tarsha <a.tarsha@yahoo.com>
2021-03-21 14:54:36 -0500

    added test files configuration

---
+++ controllers/blog.controller.test.js
@@ -0,0 +1,5 @@
+console.log("blog.controller.test.js");
+let laugh = 'Ha';
+test('the best flavor is grapefruit', () => {
+  expect(laugh).toBe('Ha');
+});
---
+++ tests/int/an-integration.test.js
@@ -0,0 +1,5 @@
+console.log("an-integration.test.js");
+let laugh = 'Ha';
+test('the best flavor is grapefruit', () => {
+  expect(laugh).toBe('Ha');
+});

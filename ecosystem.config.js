7e9a7b35878a7022deecc4a0711969a49f86bbfa
Albir Tarsha <a.tarsha@yahoo.com>
2021-03-22 21:00:39 -0500

    Update ecosystem.config.js

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,34 +1,37 @@
 module.exports = {
-  apps : [{
-    name: 'onetest',
-    script: 'server.js',
-    error_file: 'onetest_err.log',
-    out_file: 'onetest_out.log',
-    watch: '.',
-    increment_var: 'PORT',
-    env: {
-      "PORT":3000,
-      "NODE_ENV": "development"
+  apps: [
+    {
+      name: 'onetest',
+      script: 'server.js',
+      error_file: 'onetest_err.log',
+      out_file: 'onetest_out.log',
+      watch: '.',
+      increment_var: 'PORT',
+      env: {
+        PORT: 3000,
+        NODE_ENV: 'development',
+      },
+      env_production: {
+        PORT: 3000,
+        NODE_ENV: 'production',
+      },
+      // }, {
+      //   script: './service-worker/',
+      //   watch: ['./service-worker']
     },
-    env_production: {
-      "PORT":3000,
-      "NODE_ENV": "production"
-    },
-  }, {
-    script: './service-worker/',
-    watch: ['./service-worker']
-  }],
+  ],
 
-  deploy : {
-    production : {
-      user : 'SSH_USERNAME',
-      host : 'SSH_HOSTMACHINE',
-      ref  : 'origin/master',
-      repo : 'GIT_REPOSITORY',
-      path : 'DESTINATION_PATH',
+  deploy: {
+    production: {
+      user: 'SSH_USERNAME',
+      host: 'SSH_HOSTMACHINE',
+      ref: 'origin/master',
+      repo: 'GIT_REPOSITORY',
+      path: 'DESTINATION_PATH',
       'pre-deploy-local': '',
-      'post-deploy' : 'npm install && pm2 reload ecosystem.config.js --env production',
-      'pre-setup': ''
-    }
-  }
-};
+      'post-deploy':
+        'npm install && pm2 reload ecosystem.config.js --env production',
+      'pre-setup': '',
+    },
+  },
+}
